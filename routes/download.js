ff8074d7d07f696b0408611629f5342a07bd7727
Madhukumar Paka <74057522+madwhopaka@users.noreply.github.com>
2021-12-18 13:40:42 +0530

    Update download.js

--- routes/download.js
+++ routes/download.js
@@ -11,7 +11,7 @@ router.get('/:uuid', async (req,res)=> {
     if(!file) return res.render('download', {error: "Link expire ho gya"});
     console.log(file)
     var __dirname = path.resolve() ; 
-    const filePath = `app/${file.path}`;
+    const filePath = `/${file.path}`;
     console.log(filePath) ; 
     res.download(filePath) ; 
     
